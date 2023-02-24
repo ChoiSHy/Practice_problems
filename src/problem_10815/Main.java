5c246f25b03c80bfe9146d913439e560161603a4
ChoiSHy <tjdgus4697@gmail.com>
2023-02-25 02:45:17 +0900

    10815, 10816, 10845, 11003, 11066, 2263 추가

---
+++ src/problem_10815/Main.java
@@ -0,0 +1,41 @@
+package problem_10815;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        HashSet<Integer> set = new HashSet<>();
+
+        int n = 0, m = 0;
+        try {
+            n = Integer.parseInt(br.readLine());
+            for(String tok : br.readLine().split(" "))
+                set.add(Integer.parseInt(tok));
+
+            m = Integer.parseInt(br.readLine());
+            for(String tok : br.readLine().split(" ")){
+                int num = Integer.parseInt(tok);
+                if(set.contains(num))
+                    bw.write(1+" ");
+                else
+                    bw.write(0+" ");
+            }
+
+            bw.flush();
+            bw.close();
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
